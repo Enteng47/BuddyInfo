51ce01d6e66ae82a80a85a1451a088f7fe54a57a
Enteng47 <Enteng47@github.com>
2018-09-18 11:50:24 -0400

    I added comments to the code.

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,18 +1,28 @@
 import java.util.*;
 public class AddressBook {
-
+	/** The array that all the buddy's will be stored in. */
 	private ArrayList<BuddyInfo> array;
 
+	/**
+	 * The Constructor for a new AddressBook.
+	 */
 	public AddressBook() {
 		array = new ArrayList<BuddyInfo>();
 	}
-
+	/**
+	 * Adds a buddy to the AddressBook.
+	 * @param buddy the buddy to be added to the AddressBook.
+	 */
 	private void addBuddy(BuddyInfo buddy) {
 		if(buddy != null) {
 			array.add(buddy);
 		}
 	}
 
+	/**
+	 * Removes the buddy at the index.
+	 * @param index the index containing the buddy to be deleted.
+	 */
 	private void removeBuddy(int index) {
 		if(index >= 0 && index < array.size()) {
 			array.remove(index);
